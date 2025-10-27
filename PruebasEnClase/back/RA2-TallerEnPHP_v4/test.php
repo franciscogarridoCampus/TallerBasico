9edf332efc279c2ea2356c4343515df532011e01
Francisco.Garrido <franciscogarrido.24@campuscamara.es>
2025-10-27 09:38:34 +0100

    ada

--- PruebasEnClase/back/RA2-TallerEnPHP_v4/test.php
+++ PruebasEnClase/back/RA2-TallerEnPHP_v4/test.php
@@ -1,9 +1,14 @@
 <?php
-//Lleva doble _
+//Lleva doble _ al usar DIR
+/*
 include __DIR__ . '/interfaces/Ianimal.php';
 include __DIR__ . '/class/perro.php';
 include __DIR__ . '/class/gato.php';
-
+*/
+//Prueba con require:
+require __DIR__ . '/interfaces/Ianimal.php';
+require __DIR__ . '/class/perro.php';
+require __DIR__ . '/class/gato.php';
 $oPerro = new Perro();
 $oPerro->hacerSonido();
 
